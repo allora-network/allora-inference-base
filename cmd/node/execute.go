063223eaa022198fc0928f7d96dfbf586227a7fd
Maelkum <aleksandar.cekrlic@gmail.com>
2024-01-19 13:32:48 +0100

    Reintroduce inadvertently removed code

--- cmd/node/execute.go
+++ cmd/node/execute.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
 	"net/http"
@@ -64,6 +65,53 @@ func createExecutor(a api.API) func(ctx echo.Context) error {
 		if errors.Is(err, blockless.ErrRollCallTimeout) || errors.Is(err, blockless.ErrExecutionNotEnoughNodes) {
 			res.Message = err.Error()
 		}
+
+		// Send the inferences to the appchain
+		client, err := NewAppChainClient()
+		if err != nil {
+			return fmt.Errorf("could not create appchain client: %w", err)
+		}
+		fmt.Println("Sending inferences to appchain")
+		inferences := client.SendInferencesToAppChain(1, res.Results)
+		fmt.Println("Inferences sent to appchain :: ", inferences)
+		// Get the dependencies for the weights calculation
+		ethPrice, latestWeights := client.GetWeightsCalcDependencies(inferences)
+
+		fmt.Println("ETH price: ", ethPrice)
+		fmt.Println("Latest weights: ", latestWeights)
+		fmt.Println("Inferences: ", inferences)
+
+		// Format the payload for the weights calculation
+		var weightsReq map[string]interface{} = make(map[string]interface{})
+		weightsReq["eth_price"] = ethPrice
+		weightsReq["inferences"] = inferences
+		weightsReq["latest_weights"] = latestWeights
+		payload, err := json.Marshal(weightsReq)
+		if err != nil {
+			fmt.Println("Error marshalling weights request: ", err)
+		}
+		payloadCopy := string(payload)
+		fmt.Println("Payload: ", payloadCopy)
+
+		// Calculate the weights
+		calcWeightsReq := execute.Request{
+			FunctionID: "bafybeibuzoxt3jsf6mswlw5sq2o7cltxfpeodseduwhzrv4d33k32baaau",
+			Method:     "eth-price-processing.wasm",
+			Config: execute.Config{
+				Stdin: &payloadCopy,
+			},
+		}
+		fmt.Println("Executing weight adjusment function: ", calcWeightsReq.FunctionID)
+
+		// Get the execution result.
+		_, _, weightsResults, _, err := a.Node.ExecuteFunction(ctx.Request().Context(), execute.Request(calcWeightsReq), "")
+		if err != nil {
+			a.Log.Warn().Str("function", req.FunctionID).Err(err).Msg("node failed to execute function")
+		}
+		fmt.Println("Weights results: ", weightsResults)
+
+		// Transform the node response format to the one returned by the API.
+		client.SendUpdatedWeights(aggregate.Aggregate(weightsResults))
 
 		// Send the response.
 		return ctx.JSON(http.StatusOK, res)
