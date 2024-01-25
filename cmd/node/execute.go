6c51ca3029d7e5616f59810f18c00118f91d0582
Derek Anderson <dmikey@users.noreply.github.com>
2024-01-25 17:57:40 -0600

    updates and disables the chain tx if unable to connect to an rpc (#33)
    going to smash this one through, but removes the requirement to connect
    to the app chain when running the node.
    
    ```
    {"level":"info","component":"node","request":"1340ceab-d04e-45fa-8849-5d8a537b04a5","function":"bafybeiaugwh3mktzbnudurzk7jcyvmdhyy6jnairiu2phuf2t7c7orowjq","node_count":1,"cluster_size":1,"responded":1,"time":"2024-01-25T17:51:42-06:00","message":"received execution responses"}
    {"level":"debug","time":"2024-01-25T17:51:46-06:00","message":"inference results would have been submitted to chain"}
    ```

--- cmd/node/execute.go
+++ cmd/node/execute.go
@@ -37,6 +37,51 @@ type ExecuteResult struct {
 	RequestID string                `json:"request_id,omitempty"`
 }
 
+func sendResultsToChain(ctx echo.Context, a api.API, appChainClient AppChain, req ExecuteRequest, res ExecuteResponse){
+	a.Log.Info().Msg("Sending inferences to appchain")
+	inferences := appChainClient.SendInferencesToAppChain(1, res.Results)
+	a.Log.Debug().Any("inferences", inferences).Msg("Inferences sent to appchain")
+	// Get the dependencies for the weights calculation
+	ethPrice, latestWeights := appChainClient.GetWeightsCalcDependencies(inferences)
+
+	a.Log.Debug().Float64("ETH price: ", ethPrice)
+	a.Log.Debug().Float64("eth price", ethPrice)
+	a.Log.Debug().Any("latest weights", latestWeights)
+	a.Log.Debug().Any("inferences", inferences)
+
+	// Format the payload for the weights calculation
+	var weightsReq map[string]interface{} = make(map[string]interface{})
+	weightsReq["eth_price"] = ethPrice
+	weightsReq["inferences"] = inferences
+	weightsReq["latest_weights"] = latestWeights
+	payload, err := json.Marshal(weightsReq)
+	if err != nil {
+		a.Log.Error().Err(err).Msg("error marshalling weights request")
+	}
+	payloadCopy := string(payload)
+	a.Log.Debug().Any("payload: ", payloadCopy)
+
+	// Calculate the weights
+	calcWeightsReq := execute.Request{
+		FunctionID: "bafybeibuzoxt3jsf6mswlw5sq2o7cltxfpeodseduwhzrv4d33k32baaau",
+		Method:     "eth-price-processing.wasm",
+		Config: execute.Config{
+			Stdin: &payloadCopy,
+		},
+	}
+	a.Log.Debug().Any("Executing weight adjusment function: ", calcWeightsReq.FunctionID)
+
+	// Get the execution result.
+	_, _, weightsResults, _, err := a.Node.ExecuteFunction(ctx.Request().Context(), execute.Request(calcWeightsReq), "")
+	if err != nil {
+		a.Log.Warn().Str("function", req.FunctionID).Err(err).Msg("node failed to execute function")
+	}
+	a.Log.Debug().Any("weights results", weightsResults)
+
+	// Transform the node response format to the one returned by the API.
+	appChainClient.SendUpdatedWeights(aggregate.Aggregate(weightsResults))
+}
+
 func createExecutor(a api.API, appChainClient AppChain) func(ctx echo.Context) error {
 	return func(ctx echo.Context) error {
 
@@ -47,7 +92,7 @@ func createExecutor(a api.API, appChainClient AppChain) func(ctx echo.Context) e
 			return echo.NewHTTPError(http.StatusBadRequest, fmt.Errorf("could not unpack request: %w", err))
 		}
 
-		a.Log.Debug().Str("Executing inference function: ", req.FunctionID)
+		a.Log.Debug().Str("executing inference function: ", req.FunctionID)
 
 		// Get the execution result.
 		code, id, results, cluster, err := a.Node.ExecuteFunction(ctx.Request().Context(), req.Request, req.Subgroup)
@@ -68,48 +113,13 @@ func createExecutor(a api.API, appChainClient AppChain) func(ctx echo.Context) e
 			res.Message = err.Error()
 		}
 
-		a.Log.Info().Msg("Sending inferences to appchain")
-		inferences := appChainClient.SendInferencesToAppChain(1, res.Results)
-		a.Log.Debug().Any("inferences", inferences).Msg("Inferences sent to appchain")
-		// Get the dependencies for the weights calculation
-		ethPrice, latestWeights := appChainClient.GetWeightsCalcDependencies(inferences)
-
-		a.Log.Debug().Float64("ETH price: ", ethPrice)
-		a.Log.Debug().Float64("eth price", ethPrice)
-		a.Log.Debug().Any("latest weights", latestWeights)
-		a.Log.Debug().Any("inferences", inferences)
-
-		// Format the payload for the weights calculation
-		var weightsReq map[string]interface{} = make(map[string]interface{})
-		weightsReq["eth_price"] = ethPrice
-		weightsReq["inferences"] = inferences
-		weightsReq["latest_weights"] = latestWeights
-		payload, err := json.Marshal(weightsReq)
-		if err != nil {
-			a.Log.Error().Err(err).Msg("error marshalling weights request")
+		// Might be disabled if so we should log out
+		if(appChainClient.Config.SubmitTx) {
+			// don't block the return to the consumer to send these to chain
+			go sendResultsToChain(ctx, a, appChainClient, req, res)
+		} else {
+			appChainClient.Config.Logger.Debug().Msg("inference results would have been submitted to chain")
 		}
-		payloadCopy := string(payload)
-		a.Log.Debug().Any("payload: ", payloadCopy)
-
-		// Calculate the weights
-		calcWeightsReq := execute.Request{
-			FunctionID: "bafybeibuzoxt3jsf6mswlw5sq2o7cltxfpeodseduwhzrv4d33k32baaau",
-			Method:     "eth-price-processing.wasm",
-			Config: execute.Config{
-				Stdin: &payloadCopy,
-			},
-		}
-		a.Log.Debug().Any("Executing weight adjusment function: ", calcWeightsReq.FunctionID)
-
-		// Get the execution result.
-		_, _, weightsResults, _, err := a.Node.ExecuteFunction(ctx.Request().Context(), execute.Request(calcWeightsReq), "")
-		if err != nil {
-			a.Log.Warn().Str("function", req.FunctionID).Err(err).Msg("node failed to execute function")
-		}
-		a.Log.Debug().Any("weights results", weightsResults)
-
-		// Transform the node response format to the one returned by the API.
-		appChainClient.SendUpdatedWeights(aggregate.Aggregate(weightsResults))
 
 		// Send the response.
 		return ctx.JSON(http.StatusOK, res)
