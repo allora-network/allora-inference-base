5a509a409c890ac1a787f3301414ebd2f8c31e99
Diego Campo <xmariachi@gmail.com>
2024-02-14 18:02:25 +0100

    Fix empty functionType

--- cmd/node/execute.go
+++ cmd/node/execute.go
@@ -42,10 +42,10 @@ type ExecuteResult struct {
 func sendResultsToChain(ctx echo.Context, a api.API, appChainClient AppChain, req ExecuteRequest, res ExecuteResponse) {
 
 	// Only in weight functions that we will have a "type" in the response
-	functionType := "inferences"
 	functionType, err := getResponseInfo(res.Results[0].Result.Stdout)
 	if err != nil {
 		a.Log.Warn().Str("function", req.FunctionID).Err(err).Msg("node failed to extract response info from stdout")
+		functionType = "inferences"
 	}
 
 	var topicId uint64 = 0
